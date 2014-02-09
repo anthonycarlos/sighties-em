933ca9db8995967331b61521401e3deaf2441816
anthonycarlos <anthony@digitalphenom.com>
2014-02-09 08:37:15 -0500

    Add detail info to template and create card object.

---
+++ js/models/card.js
@@ -0,0 +1,49 @@
+Sighties.Card = DS.Model.extend({
+  name: DS.attr('string')
+});
+
+Sighties.List.FIXTURES = [
+  {
+    id: 1,
+    name: "said"
+  },
+  {
+    id: 2,
+    name: "it"
+  },
+  {
+    id: 3,
+    name: "is"
+  },
+  {
+    id: 4,
+    name: "he"
+  },
+  {
+    id: 5,
+    name: "am"
+  },
+  {
+    id: 6,
+    name: "I"
+  },
+  {
+    id: 7,
+    name: "come"
+  },
+  {
+    id: 8,
+    name: "here"
+  },
+  {
+    id: 9,
+    name: "no"
+  },
+  {
+    id: 10,
+    name: "on"
+  }
+];
+
+
+
