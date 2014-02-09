2b3501ce8c238ee52bafa4f1e8be88ad00ef51f5
anthonycarlos <anthony@digitalphenom.com>
2014-02-09 00:46:42 -0500

    Nest routes.

--- js/router.js
+++ js/router.js
@@ -1,9 +1,29 @@
-Sighties.Router.map(function () {
-  this.resource('lists', { path: '/' });
+Sighties.Router.map(function() {
+  this.resource('lists', function() {
+    this.resource('list', { path: ':list_id' });
+  });
+});
+
+Sighties.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('list', 1);
+  }
 });
 
 Sighties.ListsRoute = Ember.Route.extend({
-  model: function () {
+  model: function() {
     return this.store.find('list');
+  }
+});
+
+Sighties.ListRoute = Ember.Route.extend({
+  setupController: function(controller, model) {
+    controller.set('model', model);
+    //debugger;
+    console.log("ListRoute here. My model id=" + model.get('id'));
+    //controller.parentController.set('selectedList', model.get('id'));
+  },
+  model: function(params) {
+    return Sighties.List.store.find('list', params.list_id);
   }
 });
