0426b62d5d78145294f39dd2e21b75bd1caca3cd
Anthony Carlos <anthony@carlosfamily.com>
2014-02-16 11:48:00 -0500

    First cut at adding new lists. Hard code isEditing.

--- js/controllers/lists_controller.js
+++ js/controllers/lists_controller.js
@@ -1,7 +1,27 @@
 Sighties.ListsController = Ember.ArrayController.extend({
   // TODO: Figure out a way to initialize this value
+  isCreating: true,
   selectedList: undefined,
   actions: {
+    createList: function() {
+      // Get the list name set by the "New List Name" text field
+      var name = this.get('newListName');
+      if (!name.trim()) { return; }
+
+      // Create the new List model
+      var list = this.store.createRecord('list', {
+        name: name,
+        canEdit: true,
+        cards: []
+      });
+
+      // Clear the "New List Name" text field
+      this.set('newListName', '');
+
+      // Save the new model
+      list.save();
+    }
+
   }
 });
 
